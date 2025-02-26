6755ed27067de554b9187e697e7564de6fb00d0a
tzuk999 <tzuk999@gmail.com>
2025-02-26 18:56:14 +0200

    added base dao

---
+++ src/dal/baseDAO.py
@@ -0,0 +1,40 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+class BaseDAO:
+    def __init__(self, connection_params, table_name):
+        self.connection_params = connection_params
+        self.table_name = table_name
+
+    def _get_connection(self):
+        return psycopg2.connect(**self.connection_params)
+
+    def read_all(self):
+        with self._get_connection() as conn:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                cur.execute(f"SELECT * FROM {self.table_name}")
+                return cur.fetchall()
+
+    def add_row(self, data):
+        columns = ', '.join(data.keys())
+        values = ', '.join(['%s'] * len(data))
+        with self._get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    f"INSERT INTO {self.table_name} ({columns}) VALUES ({values}) RETURNING id",
+                    tuple(data.values())
+                )
+                conn.commit()
+                return cur.fetchone()[0]
+
+    def read_by_id(self, row_id):
+        with self._get_connection() as conn:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                cur.execute(f"SELECT * FROM {self.table_name} WHERE id = %s", (row_id,))
+                return cur.fetchone()
+
+    def delete_by_id(self, row_id):
+        with self._get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"DELETE FROM {self.table_name} WHERE id = %s", (row_id,))
+                conn.commit()
